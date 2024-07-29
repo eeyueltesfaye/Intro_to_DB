05b57c186db1d996273dfeea925c456b26079ec3
Eyuel Tesfaye <163458107+eeyueltesfaye@users.noreply.github.com>
2024-07-30 00:14:16 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,15 +1,12 @@
 
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-
 USE alx_book_store;
-
 
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
-
 
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -20,7 +17,6 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -28,14 +24,12 @@ CREATE TABLE Customers (
     address TEXT
 );
 
-
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-
 
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
